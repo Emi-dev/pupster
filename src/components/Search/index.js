2b2d768422ed323f727984deadf0565fea0b85fb
Emi Clar <kotaki@gmail.com>
2020-04-23 21:02:27 -0400

    added some files.

---
+++ src/components/Search/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./style.css";
+import API from "../../utils/API";
+
+class Search extends React.Component {
+    state = {
+        search: ""
+    }
+}
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,1 +1,8 @@
 import axios from "axios";
+
+
+export default {
+    search: function(props) {
+        return axios.get(`https://dog.ceo/api/breed/${props.breed}/images`);
+    }
+};
